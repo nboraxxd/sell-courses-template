fdaf4336ffce1b794c702a387fca691ad0edb5b0
bora <nhatbaoxxd@gmail.com>
2023-08-24 20:50:53 +0700

    UI Header component
    - Handle header UI
    - Handle popover by Floating UI
    - Install prettier-plugin-tailwindcss package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line no-undef
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
@@ -7,6 +10,21 @@ export default {
         avo: ['SVN-Avo', 'sans-serif'],
       },
     },
+    backgroundImage: {
+      'down-arrow': "url('@/assets/images/down-arrow.svg')",
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        '.container': {
+          maxWidth: '83rem',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: theme('spacing.4'),
+          paddingRight: theme('spacing.4'),
+        },
+      })
+    }),
+  ],
 }
